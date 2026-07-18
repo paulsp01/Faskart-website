994de38577fa188cefea53aad99e414f570325cb
paulsp01 <paulswarnalee@gmail.com>
2026-07-18 22:54:53 +0530

    updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
             <h1 className='text-2xl lg:text-3xl xl:text-5xl font-bold '>Premium fashion<br/> wear confidence everyday</h1>
             <p className="text-xs lg:text-sm xl:text-md">Discover the latest trends with premium quality<br/> and affordable pricing</p>
             <div className='flex flex-col sm:flex-row gap-3 text-xs lg:text-base xl:text-md'>
-                <button className='bg-[#3B2F2F] text-white w-fit p-3 rounded-md lg:text-lg lg:p-4'>Shop Now</button>
-                <button className='border border-[#3B2F2F] p-2 w-fit rounded-md lg:text-lg lg:p-4'>Explore Collection</button>
+                <button className='bg-[#3B2F2F] text-white w-fit p-3 rounded-md xl:text-lg xl:p-4'>Shop Now</button>
+                <button className='border border-[#3B2F2F] p-2 w-fit rounded-md xl:text-lg xl:p-4'>Explore Collection</button>
             </div>
         </div>
        
